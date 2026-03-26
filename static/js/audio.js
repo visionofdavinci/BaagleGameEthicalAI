4a2cafb2cba757e1c7784d466ea6ede260624e08
visionofdavinci <145295500+visionofdavinci@users.noreply.github.com>
2026-03-26 13:17:20 +0100

    Made last push for music

--- static/js/audio.js
+++ static/js/audio.js
@@ -1,13 +1,10 @@
 /* background music manager - loops SoundGame2 until midmorning_check, then switches to SoundGame1 */
 
 window.AudioManager = (function () {
-  const track1 = new Audio('SoundGame1.mpeg');
-  const track2 = new Audio('SoundGame2.mpeg');
+  const track1 = new Audio('FinalSoundGame.mpeg');
 
   track1.loop = true;
-  track2.loop = true;
-  track1.volume = 0.3;
-  track2.volume = 0.3;
+  track1.volume = 0.4;
 
   let started = false;
   let currentTrack = null;
@@ -15,14 +12,14 @@ window.AudioManager = (function () {
   function startMusic() {
     if (started) return;
     started = true;
-    currentTrack = track2;
-    track2.play().catch(function () {});
+    currentTrack = track1;
+    track1.play().catch(function () {});
   }
 
   function switchToTrack1() {
     if (currentTrack === track1) return;
-    track2.pause();
-    track2.currentTime = 0;
+    track1.pause();
+    track1.currentTime = 0;
     currentTrack = track1;
     track1.play().catch(function () {});
   }
